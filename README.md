9c37a6cf9241c4d3b7436158510f3c666e267e27
haha1994 <934654332@qq.com>
2021-08-26 21:52:31 +0800

    [k8s] finish the first version of init shell script

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ docker images transfer
 
 ## CentOS 7 配置方法
 https://github.com/haha1994/docker_images/blob/main/k8s/v1.21.2/new-server-config-steps.txt
+
+
+# 附录
+
+# 附录一 记录如何使用阿里云镜像构建的功能，拉取外网镜像
+# TODO
+
+# 附录二 coredns 拉取失败的问题
+# 实际镜像为：k8s.gcr.io/coredns/coredns:v1.8.0
+# 换源后会自动缺省目录，所以换源后的镜像地址应该是 aliyun.com/fxhaha/coredns:v1.8.0，相对与原地址少了一层/coredns
--- k8s/v1.21.2/new-server-master-init.sh
+++ k8s/v1.21.2/new-server-master-init.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
-#新服务器配置脚本
-#关闭防火墙
+# 新服务器配置脚本
+# 关闭防火墙
 systemctl stop firewalld
 systemctl disable firewalld
 
-#安装iptables
+# 安装iptables
 yum -y install iptables-services && systemctl start iptables && systemctl enable iptables && iptables -F && service iptables save
 
-#关闭 SELINUX
+# 关闭 SELINUX
 swapoff -a && sed -i 's/.*swap.*/#&/' /etc/fstab
 setenforce 0 && sed -i 's/^SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
 
-#设置时间同步
+# 设置时间同步
 yum -y install ntpdate
 ntpdate pool.ntp.org
 
-#centos7
+# centos7
 mkdir /home/k8s
 cd /home/k8s
 wget https://raw.githubusercontent.com/haha1994/docker_images/main/k8s/v1.21.2/etc_modules-load.d_k8s.conf
@@ -29,7 +29,7 @@ sudo sysctl --system
 wget https://raw.githubusercontent.com/haha1994/docker_images/main/k8s/v1.21.2/etc_yum.repos.d_kubernetes.repo
 cp ./etc_yum.repos.d_kubernetes.repo /etc/yum.repos.d/kubernetes.repo
 
-#master上执行：
+# master上执行：
 sudo yum -y install kubeadm-1.21.2 kubelet-1.21.2 kubectl-1.21.2
 sudo systemctl enable --now kubelet && systemctl start kubelet
 
@@ -46,7 +46,7 @@ curl -fsSL get.docker.com -o get-docker.sh
 # 确定安装版本20.10
 VERSION=20.10 sh get-docker.sh --mirror Aliyun
 
-#配置 daemon.json
+# 配置 daemon.json
 mkdir /etc/docker
 
 # 修改docker cgroup
@@ -55,7 +55,7 @@ cp ./etc_docker_daemon.json /etc/docker/daemon.json
 
 mkdir -p /etc/systemd/system/docker.service.d
 
-#重启docker服务
+# 重启docker服务
 systemctl daemon-reload
 systemctl restart docker
 systemctl enable docker
@@ -64,10 +64,10 @@ systemctl enable docker
 # k8s中，如果用户没有在 KubeletConfiguration 中设置 cgroupDriver 字段， kubeadm init 会将它设置为默认值 systemd
 
 
-# TODO 需要管理员输入IP
-read -p "Please enter the IP address of this machine:" ipaddress
+# 需要管理员输入本机IP
+read -p "Please enter the IP address of this machine:" youripaddress
+
 #打印kubeadm初始化配置
-cd /home/k8s
 #kubeadm config print init-defaults > kubeadm-config.yaml
 ##修改其中信息
 #advertiseAddress: 192.168.127.10 #master节点的IP
@@ -76,51 +76,48 @@ cd /home/k8s
 #kubernetesVersion: 1.21.2 #Kubernetes软件版本
 #podSubnet: 10.244.0.0/16 #networking下添加这个内容
 
-#预先下载镜像
+cd /home/k8s
+wget https://raw.githubusercontent.com/haha1994/docker_images/main/k8s/v1.21.2/kubeadm-config.yaml
+# 替换IP地址
+sed -i "s/youripaddress/${youripaddress}/g" kubeadm-config.yaml
+
+# 预先下载镜像
 kubeadm config images pull --config kubeadm-config.yaml
 
-#初始化节点，如果kubeadm初始化失败了，一定要kubeadm reset
+# 初始化节点，如果kubeadm初始化失败了，一定要kubeadm reset
 kubeadm init --config=kubeadm-config.yaml | tee kubeadm-init.log
 
+# 将k8s配置路径添加到环境变量
 export KUBECONFIG=/etc/kubernetes/admin.conf
-# TODO 这个需要添加到环境变量
+echo 'export KUBECONFIG=/etc/kubernetes/admin.conf' >> /etc/profile
 
-# 安装网络插件flannel，官网地址https://github.com/flannel-io/flannel
+# 安装网络插件flannel v0.14.0，官网地址https://github.com/flannel-io/flannel
 # 官方Readme中的地址也写错了，没有更新为最新地址（如下）
-wget https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+# wget https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+wget https://raw.githubusercontent.com/haha1994/docker_images/main/kube-flannel/v0.14.0/kube-flannel.yml
 kubectl apply -f kube-flannel.yml
 
-#将主节点做为工作节点
+# 将主节点做为工作节点
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
+# node
 #加入工作节点
 #kubeadm join --token <token> <master-ip>:<master-port> --discovery-token-ca-cert-hash sha256:<hash>
-#
-##安装网络插件
+##node安装网络插件
 #wget https://github.com/flannel-io/flannel/blob/master/Documentation/kube-flannel.yml
-#
 #kubectl apply -f kube-flannel.yml
-#
-##安装ingress-nginx-controller
-## TODO
-#wget https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/baremetal/deploy.yaml
-#
-## 修改 ingress-nginx-controller 的 Deployment
+# edon
+
+# 安装ingress-nginx-controller v0.47.0
+# 官网 wget https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/baremetal/deploy.yaml
+# 修改 ingress-nginx-controller 的 Deployment
 #kind: DaemonSet # modify
 #spec:
 #  template:
 #    spec:
 #      hostNetwork: true # add 
 #      dnsPolicy: ClusterFirstWithHostNet # modify
-#
-#kubectl apply -f ingress-nginx.yaml
-#kubectl delete -A validatingwebhookconfiguration ingress-nginx-admission
+wget https://raw.githubusercontent.com/haha1994/docker_images/main/nginx-ingress-controller/v0.47.0/ingress-nginx.yaml
 
-#附录
-
-# 附录一 记录如何使用阿里云镜像构建的功能，拉取外网镜像
-# TODO
-
-# 附录二 coredns 拉取失败的问题
-# 实际镜像为：k8s.gcr.io/coredns/coredns:v1.8.0
-# 换源后会自动缺省目录，所以换源后的镜像地址应该是 aliyun.com/fxhaha/coredns:v1.8.0，相对与原地址少了一层/coredns
+kubectl apply -f ingress-nginx.yaml
+kubectl delete -A validatingwebhookconfiguration ingress-nginx-admission
